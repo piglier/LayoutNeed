f5b0fce7312e0f4a1f04a926b0ccd579491cd095
阿朱 <pig2000b@firstins.com.tw>
2022-11-30 23:48:31 +0800

    修正 用swiftFormat調整排版

--- LayoutNeed/LayoutNeed/ViewController.swift
+++ LayoutNeed/LayoutNeed/ViewController.swift
@@ -8,47 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let adjustButton = UIButton()
+    let redView = UIView()
     
-    let adjustButton = UIButton();
-    let redView = UIView();
-    
-    var animator = UIViewPropertyAnimator();
-    var progress: CGFloat = 0.0;
+    var animator = UIViewPropertyAnimator()
+    var progress: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addSubView();
-        constrains();
-        setupUI();
+        addSubView()
+        constrains()
+        setupUI()
         
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)));
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
         
-        view.addGestureRecognizer(gesture);
-        animator = UIViewPropertyAnimator.init(duration: 2, curve: .easeOut, animations: {
-            self.redView.frame = self.redView.frame.offsetBy(dx: 200, dy: 0);
+        view.addGestureRecognizer(gesture)
+        animator = UIViewPropertyAnimator(duration: 2, curve: .easeOut, animations: {
+            self.redView.frame = self.redView.frame.offsetBy(dx: 200, dy: 0)
         })
         
-        adjustButton.addTarget(self, action: #selector(myAnimate), for: .touchUpInside);
+        adjustButton.addTarget(self, action: #selector(myAnimate), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
     override func viewDidLayoutSubviews() {
-        redView.layer.cornerRadius = redView.frame.width / 2;
+        redView.layer.cornerRadius = redView.frame.width / 2
     }
     
     @objc
     func handlePan(recognizer: UIPanGestureRecognizer) {
         switch recognizer.state {
         case .began:
-            animator.pauseAnimation();
-            progress = animator.fractionComplete;
+            animator.pauseAnimation()
+            progress = animator.fractionComplete
         case .changed:
-            let translation = recognizer.translation(in: self.redView);
-            animator.fractionComplete = translation.x / 200 + progress;
+            let translation = recognizer.translation(in: redView)
+            animator.fractionComplete = translation.x / 200 + progress
         case .ended:
-            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0);
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
         default:
-            break;
+            break
         }
     }
     
@@ -67,39 +66,36 @@ class ViewController: UIViewController {
 //        animator.startAnimation();
         
         /// CALayer
-        let animation = CABasicAnimation.init(keyPath: "position.x");
-        animation.fromValue = self.redView.center.x;
-        animation.toValue = self.redView.center.x + 200;
-        animation.duration = 2;
-        self.redView.layer.add(animation, forKey: nil);
+        let animation = CABasicAnimation(keyPath: "position.x")
+        animation.fromValue = redView.center.x
+        animation.toValue = redView.center.x + 200
+        animation.duration = 2
+        redView.layer.add(animation, forKey: nil)
     }
     
     func addSubView() {
-        self.view.addSubview(adjustButton);
-        self.view.addSubview(redView);
+        view.addSubview(adjustButton)
+        view.addSubview(redView)
     }
     
     func constrains() {
-        adjustButton.translatesAutoresizingMaskIntoConstraints = false;
-        adjustButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 50, paddingBottom: 100, paddingRight: 50, width: 0, height: 50, enableInsets: false);
+        adjustButton.translatesAutoresizingMaskIntoConstraints = false
+        adjustButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 50, paddingBottom: 100, paddingRight: 50, width: 0, height: 50, enableInsets: false)
         
-        redView.translatesAutoresizingMaskIntoConstraints = false;
-        redView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 100, paddingLeft: 50, paddingBottom: 0, paddingRight: 0, width: 100, height: 100, enableInsets: false);
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 100, paddingLeft: 50, paddingBottom: 0, paddingRight: 0, width: 100, height: 100, enableInsets: false)
     }
     
     func setupUI() {
-        adjustButton.backgroundColor = .systemBlue;
-        adjustButton.setTitle("Animate", for: .normal);
-        redView.backgroundColor = .systemRed;
-        redView.clipsToBounds = true;
-
+        adjustButton.backgroundColor = .systemBlue
+        adjustButton.setTitle("Animate", for: .normal)
+        redView.backgroundColor = .systemRed
+        redView.clipsToBounds = true
     }
-    
 }
 
-
 extension UIView {
-    func anchor (top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat, enableInsets: Bool) {
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat, enableInsets: Bool) {
         var topInset = CGFloat(0)
         var bottomInset = CGFloat(0)
         
@@ -107,24 +103,21 @@ extension UIView {
             let insets = self.safeAreaInsets
             topInset = insets.top
             bottomInset = insets.bottom
-            
-            print("Top: \(topInset)")
-            print("bottom: \(bottomInset)")
         }
         
         translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
-            self.topAnchor.constraint(equalTo: top, constant: paddingTop+topInset).isActive = true
+            topAnchor.constraint(equalTo: top, constant: paddingTop + topInset).isActive = true
         }
         if let left = left {
-            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
         }
         if let right = right {
             rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
         }
         if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom-bottomInset).isActive = true
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom - bottomInset).isActive = true
         }
         if height != 0 {
             heightAnchor.constraint(equalToConstant: height).isActive = true
